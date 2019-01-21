c81035bb240eaddc7fc257de66397e86ff0aeea1
Cody Hodges <cody35367@gmail.com>
2019-01-21 03:00:55 -0600

    Basic layout of the app

---
+++ app/src/main/java/com/cody35367/datetracker/SettingsMenu.kt
@@ -0,0 +1,12 @@
+package com.cody35367.datetracker
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SettingsMenu : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+    }
+}
