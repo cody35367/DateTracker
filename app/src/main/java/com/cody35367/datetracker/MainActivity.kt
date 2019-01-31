77274ad758d1ff598ec9453019f852c30cf33dec
Cody Hodges <cody35367@gmail.com>
2019-01-30 22:46:08 -0600

    Progress with the access

--- app/src/main/java/com/cody35367/datetracker/MainActivity.kt
+++ app/src/main/java/com/cody35367/datetracker/MainActivity.kt
@@ -1,20 +1,68 @@
 package com.cody35367.datetracker
 
+import com.google.android.gms.common.ConnectionResult
+import com.google.android.gms.common.GoogleApiAvailability
+import com.google.api.client.extensions.android.http.AndroidHttp
+import com.google.api.client.googleapis.extensions.android.gms.auth.GoogleAccountCredential
+import com.google.api.client.googleapis.extensions.android.gms.auth.GooglePlayServicesAvailabilityIOException
+import com.google.api.client.googleapis.extensions.android.gms.auth.UserRecoverableAuthIOException
+
+import com.google.api.client.json.jackson2.JacksonFactory
+import com.google.api.client.util.ExponentialBackOff
+
+import com.google.api.services.sheets.v4.SheetsScopes
+
+import android.Manifest
+import android.accounts.AccountManager
+import android.content.Context
 import android.content.Intent
+import android.net.ConnectivityManager
+import android.os.AsyncTask
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity;
+import android.support.annotation.NonNull
+import android.support.v7.app.AppCompatActivity
+import android.text.TextUtils
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import android.util.Log
+
+import java.io.IOException
+import java.util.ArrayList
+
+import pub.devrel.easypermissions.AfterPermissionGranted
+import pub.devrel.easypermissions.EasyPermissions
 
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
 
-class MainActivity : AppCompatActivity() {
+const val REQUEST_ACCOUNT_PICKER = 1000
+const val REQUEST_AUTHORIZATION = 1001
+const val REQUEST_GOOGLE_PLAY_SERVICES = 1002
+const val REQUEST_PERMISSION_GET_ACCOUNTS = 1003
+const val TAG = "MainActivity"
+
+class MainActivity : AppCompatActivity(), EasyPermissions.PermissionCallbacks {
+
+    private var mCredential: GoogleAccountCredential? = null
+
+    private val PREF_ACCOUNT_NAME = "accountName"
+    private val SCOPES = arrayOf(SheetsScopes.SPREADSHEETS_READONLY)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
+
+        addBtn.setOnClickListener(View.OnClickListener {
+            Log.v(TAG,"addBtn pressed!")
+            getResultsFromApi()
+        })
+        // Initialize credentials and service object.
+        mCredential = GoogleAccountCredential.usingOAuth2(
+            applicationContext, SCOPES.toList()
+        ).setBackOff(ExponentialBackOff())
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -36,5 +84,299 @@ class MainActivity : AppCompatActivity() {
     fun goToSettingsMenu(): Boolean {
         startActivity(Intent(this, SettingsMenu::class.java))
         return true
+    }
+
+    /**
+     * Checks whether the device currently has a network connection.
+     * @return true if the device has a network connection, false otherwise.
+     */
+    private val isDeviceOnline: Boolean
+        get() {
+            val connMgr = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+            val networkInfo = connMgr.getActiveNetworkInfo()
+            return networkInfo != null && networkInfo.isConnected()
+        }
+
+    /**
+     * Check that Google Play services APK is installed and up to date.
+     * @return true if Google Play Services is available and up to
+     * date on this device; false otherwise.
+     */
+    private val isGooglePlayServicesAvailable: Boolean
+        get() {
+            val apiAvailability = GoogleApiAvailability.getInstance()
+            val connectionStatusCode = apiAvailability.isGooglePlayServicesAvailable(this)
+            return connectionStatusCode == ConnectionResult.SUCCESS
+        }
+
+    /**
+     * Attempt to call the API, after verifying that all the preconditions are
+     * satisfied. The preconditions are: Google Play Services installed, an
+     * account was selected and the device currently has online access. If any
+     * of the preconditions are not satisfied, the app will prompt the user as
+     * appropriate.
+     */
+    private fun getResultsFromApi() {
+        if (!isGooglePlayServicesAvailable) {
+            acquireGooglePlayServices()
+        } else if (mCredential?.getSelectedAccountName() == null) {
+            chooseAccount()
+        } else if (!isDeviceOnline) {
+            //mOutputText?.setText("No network connection available.")
+            Log.i(TAG,"No network connection available.")
+        } else {
+            Log.v(TAG,"All sheet access conditions met!")
+            mCredential?.let {cred -> MakeRequestTask(cred).execute()}
+        }
+    }
+
+    /**
+     * Attempts to set the account used with the API credentials. If an account
+     * name was previously saved it will use that one; otherwise an account
+     * picker dialog will be shown to the user. Note that the setting the
+     * account to use with the credentials object requires the app to have the
+     * GET_ACCOUNTS permission, which is requested here if it is not already
+     * present. The AfterPermissionGranted annotation indicates that this
+     * function will be rerun automatically whenever the GET_ACCOUNTS permission
+     * is granted.
+     */
+    @AfterPermissionGranted(REQUEST_PERMISSION_GET_ACCOUNTS)
+    private fun chooseAccount() {
+        if (EasyPermissions.hasPermissions(
+                this, Manifest.permission.GET_ACCOUNTS
+            )
+        ) {
+            val accountName = getPreferences(Context.MODE_PRIVATE)
+                .getString(PREF_ACCOUNT_NAME, null)
+            if (accountName != null) {
+                mCredential?.setSelectedAccountName(accountName)
+                getResultsFromApi()
+            } else {
+                // Start a dialog from which the user can choose an account
+                startActivityForResult(
+                    mCredential?.newChooseAccountIntent(),
+                    REQUEST_ACCOUNT_PICKER
+                )
+            }
+        } else {
+            // Request the GET_ACCOUNTS permission via a user dialog
+            EasyPermissions.requestPermissions(
+                this,
+                "This app needs to access your Google account (via Contacts).",
+                REQUEST_PERMISSION_GET_ACCOUNTS,
+                Manifest.permission.GET_ACCOUNTS
+            )
+        }
+    }
+
+    /**
+     * Called when an activity launched here (specifically, AccountPicker
+     * and authorization) exits, giving you the requestCode you started it with,
+     * the resultCode it returned, and any additional data from it.
+     * @param requestCode code indicating which activity result is incoming.
+     * @param resultCode code indicating the result of the incoming
+     * activity result.
+     * @param data Intent (containing result data) returned by incoming
+     * activity result.
+     */
+    override fun onActivityResult(
+        requestCode: Int, resultCode: Int, data: Intent?
+    ) {
+        super.onActivityResult(requestCode, resultCode, data)
+        when (requestCode) {
+            REQUEST_GOOGLE_PLAY_SERVICES -> if (resultCode != RESULT_OK) {
+                //mOutputText?.setText("This app requires Google Play Services. Please install " + "Google Play Services on your device and relaunch this app.")
+                Log.i(TAG,"This app requires Google Play Services. Please install " + "Google Play Services on your device and relaunch this app.")
+            } else {
+                getResultsFromApi()
+            }
+            REQUEST_ACCOUNT_PICKER -> if (resultCode == RESULT_OK && data != null &&
+                data.getExtras() != null
+            ) {
+                val accountName = data.getStringExtra(AccountManager.KEY_ACCOUNT_NAME)
+                if (accountName != null) {
+                    val settings = getPreferences(Context.MODE_PRIVATE)
+                    val editor = settings.edit()
+                    editor.putString(PREF_ACCOUNT_NAME, accountName)
+                    editor.apply()
+                    mCredential?.setSelectedAccountName(accountName)
+                    getResultsFromApi()
+                }
+            }
+            REQUEST_AUTHORIZATION -> if (resultCode == RESULT_OK) {
+                getResultsFromApi()
+            }
+        }
+    }
+
+    /**
+     * Respond to requests for permissions at runtime for API 23 and above.
+     * @param requestCode The request code passed in
+     * requestPermissions(android.app.Activity, String, int, String[])
+     * @param permissions The requested permissions. Never null.
+     * @param grantResults The grant results for the corresponding permissions
+     * which is either PERMISSION_GRANTED or PERMISSION_DENIED. Never null.
+     */
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        @NonNull permissions: Array<String>,
+        @NonNull grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        EasyPermissions.onRequestPermissionsResult(
+            requestCode, permissions, grantResults, this
+        )
+    }
+
+    /**
+     * Callback for when a permission is granted using the EasyPermissions
+     * library.
+     * @param requestCode The request code associated with the requested
+     * permission
+     * @param list The requested permission list. Never null.
+     */
+    override fun onPermissionsGranted(requestCode: Int, list: kotlin.collections.List<String>) {
+        // Do nothing.
+    }
+
+    /**
+     * Callback for when a permission is denied using the EasyPermissions
+     * library.
+     * @param requestCode The request code associated with the requested
+     * permission
+     * @param list The requested permission list. Never null.
+     */
+    override fun onPermissionsDenied(requestCode: Int, list: kotlin.collections.List<String>) {
+        // Do nothing.
+    }
+
+    /**
+     * Attempt to resolve a missing, out-of-date, invalid or disabled Google
+     * Play Services installation via a user dialog, if possible.
+     */
+    private fun acquireGooglePlayServices() {
+        val apiAvailability = GoogleApiAvailability.getInstance()
+        val connectionStatusCode = apiAvailability.isGooglePlayServicesAvailable(this)
+        if (apiAvailability.isUserResolvableError(connectionStatusCode)) {
+            showGooglePlayServicesAvailabilityErrorDialog(connectionStatusCode)
+        }
+    }
+
+
+    /**
+     * Display an error dialog showing that Google Play Services is missing
+     * or out of date.
+     * @param connectionStatusCode code describing the presence (or lack of)
+     * Google Play Services on this device.
+     */
+    internal fun showGooglePlayServicesAvailabilityErrorDialog(
+        connectionStatusCode: Int
+    ) {
+        val apiAvailability = GoogleApiAvailability.getInstance()
+        val dialog = apiAvailability.getErrorDialog(
+            this@MainActivity,
+            connectionStatusCode,
+            REQUEST_GOOGLE_PLAY_SERVICES
+        )
+        dialog.show()
+    }
+
+    /**
+     * An asynchronous task that handles the Google Sheets API call.
+     * Placing the API calls in their own task ensures the UI stays responsive.
+     */
+    private inner class MakeRequestTask internal constructor(credential: GoogleAccountCredential) :
+        AsyncTask<Void, Void, kotlin.collections.List<String>>() {
+        private var mService: com.google.api.services.sheets.v4.Sheets? = null
+        private var mLastError: Exception? = null
+
+        /**
+         * Fetch a list of names and majors of students in a sample spreadsheet:
+         * https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
+         * @return List of names and majors
+         * @throws IOException
+         */
+        private val dataFromApi: kotlin.collections.List<String>
+            @Throws(IOException::class)
+            get() {
+                val spreadsheetId = "1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms"
+                val range = "Class Data!A2:E"
+                val results = ArrayList<String>()
+                val response = this.mService!!.spreadsheets().values()
+                    .get(spreadsheetId, range)
+                    .execute()
+                val values = response.getValues()
+                if (values != null) {
+                    results.add("Name, Major")
+                    for (row in values) {
+                        results.add(row[0].toString() + ", " + row[4])
+                    }
+                }
+                return results
+            }
+
+        init {
+            val transport = AndroidHttp.newCompatibleTransport()
+            val jsonFactory = JacksonFactory.getDefaultInstance()
+            mService = com.google.api.services.sheets.v4.Sheets.Builder(
+                transport, jsonFactory, credential
+            )
+                .setApplicationName("Google Sheets API Android Quickstart")
+                .build()
+        }
+
+        /**
+         * Background task to call Google Sheets API.
+         * @param params no parameters needed for this task.
+         */
+        override fun doInBackground(vararg params: Void): kotlin.collections.List<String>? {
+            try {
+                return dataFromApi
+            } catch (e: Exception) {
+                mLastError = e
+                cancel(true)
+                return null
+            }
+
+        }
+
+
+        override fun onPreExecute() {
+            //mOutputText?.setText("")
+            Log.i(TAG,"")
+        }
+
+        fun onPostExecute(output: kotlin.collections.MutableList<String>?) {
+            if (output == null || output.size === 0) {
+                //mOutputText?.setText("No results returned.")
+                Log.i(TAG,"No results returned.")
+            } else {
+                output.add(0, "Data retrieved using the Google Sheets API:")
+                //mOutputText?.setText(TextUtils.join("\n", output))
+                Log.i(TAG, TextUtils.join("\n", output))
+            }
+        }
+
+        override fun onCancelled() {
+            if (mLastError != null) {
+                if (mLastError is GooglePlayServicesAvailabilityIOException) {
+                    showGooglePlayServicesAvailabilityErrorDialog(
+                        (mLastError as GooglePlayServicesAvailabilityIOException)
+                            .getConnectionStatusCode()
+                    )
+                } else if (mLastError is UserRecoverableAuthIOException) {
+                    startActivityForResult(
+                        (mLastError as UserRecoverableAuthIOException).getIntent(),
+                        REQUEST_AUTHORIZATION
+                    )
+                } else {
+                    //mOutputText?.setText("The following error occurred:\n" + mLastError!!.message)
+                    Log.i(TAG,"The following error occurred:\n" + mLastError!!.message)
+                }
+            } else {
+                //mOutputText?.setText("Request cancelled.")
+                Log.i(TAG,"Request cancelled.")
+            }
+        }
     }
 }
